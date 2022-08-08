ad293e8b9376dbcd8e3c2fd0b82be580ca3eec05
Andrey Yurkovsky <andrey.yurkovsky@gmail.com>
2022-08-08 02:56:29 +0200

    fix: hide login form

--- src/ts/login.ts
+++ src/ts/login.ts
@@ -4,3 +4,9 @@ enterButton.addEventListener('click', (event) => {
   event.preventDefault();
   window.open('main.html') //fake authorization
 })
+
+const authLoginButton = document.querySelector('.auth__login') as HTMLInputElement;
+const authLoginForm = document.querySelector('form[name=login]') as HTMLFormElement;
+authLoginButton.addEventListener('click', () => {
+  authLoginForm.style.visibility = 'visible';
+})
