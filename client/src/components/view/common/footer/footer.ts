f1689a42d8fa4d3699ddaebc1dd121bef99c6a56
Mikhail Aleinik <6424156@tut.by>
2022-08-10 23:59:25 +0300

    feat: added base structure folders and files

---
+++ client/src/components/view/common/footer/footer.ts
@@ -0,0 +1,13 @@
+export default class Footer {
+    private readonly TAG_CONTAINER = 'footer';
+    private readonly CLASS_CONTAINER = 'footer';
+
+    private _footerElement = document.createElement(this.TAG_CONTAINER);
+
+    constructor() {
+        this.createFooterHtmlElement();
+    }
+    private createFooterHtmlElement() {
+        
+    }
+}
