9a6dd1e3ca37b358cfe5ff7e3ceb4ea3edbd24f2
Mikhail Aleinik <6424156@tut.by>
2022-08-16 01:12:28 +0300

    refactor: added comments in events file

--- client/src/components/controller/app-events.ts
+++ client/src/components/controller/app-events.ts
@@ -7,20 +7,24 @@
  */
 export const enum AppEvents {
     //Index page
-    AUTH_CLICK_BUTTON = 'clickAuthButton',
-    AUTH_ENABLE_BUTTON = 'enableAuthButton',
-    AUTH_DISABLE_BUTTON = 'disableAuthButton',
-    AUTH_SHOW_WINDOW = 'showAuthWindow',
-    AUTH_HIDE_WINDOW = 'hideAuthWindow',
-    AUTH_GET_AUTH_USER = 'getAuthUser',
-    REGISTER_CLICK_BUTTON = 'clickRegisterButton',
-    REGISTER_ENABLE_BUTTON = 'enableRegisterButton',
-    REGISTER_DISABLE_BUTTON = 'disableRegisterButton',
-    REGISTER_HIDE_BUTTON = 'hideRegisterButton',
-    REGISTER_SHOW_BUTTON = 'showRegisterButton',
-    REGISTER_SHOW_WINDOW = 'showRegisterWindow',
-    REGISTER_HIDE_WINDOW = 'hideRegisterWindow',
-    REGISTER_USER = 'registerUser',
-    REGISTER_USER_SUCCESS = 'registerUserSuccess',
-    REGISTER_USER_FAIL = 'registerUserFail',
+    AUTH_CLICK_BUTTON = 'clickAuthButton',//клик кнопки LogIn
+    AUTH_CLICK_LOGOUT_BUTTON = 'clickLogOutButton',//клик кнопки LogOut
+    AUTH_ENABLE_BUTTON = 'enableAuthButton',//enable кнопки LogIn
+    AUTH_DISABLE_BUTTON = 'disableAuthButton',//disable кнопки LogIn
+    AUTH_HIDE_BUTTON = 'hideAuthButton',//показать кнопку авторизации
+    AUTH_SHOW_BUTTON = 'showAuthButton',//скрыть кнопку авторизации
+    AUTH_SHOW_WINDOW = 'showAuthWindow',//показать окно авторизации
+    AUTH_HIDE_WINDOW = 'hideAuthWindow',//скрыть окно авторизации
+    AUTH_GET_AUTH_USER = 'getAuthUser',//получить текущего авторизованного пользователя
+    AUTH_LOGOUT_USER = 'userLogOut',//пользователь вышел из приложения
+    REGISTER_CLICK_BUTTON = 'clickRegisterButton',//клик кнопки регистрации
+    REGISTER_ENABLE_BUTTON = 'enableRegisterButton',//enable кнопки регистрации
+    REGISTER_DISABLE_BUTTON = 'disableRegisterButton',//disable кнопки регистрации
+    REGISTER_HIDE_BUTTON = 'hideRegisterButton',//скрыть кнопку регистрации
+    REGISTER_SHOW_BUTTON = 'showRegisterButton',//показать кнопку регистрации
+    REGISTER_SHOW_WINDOW = 'showRegisterWindow',//показать окно регистрации
+    REGISTER_HIDE_WINDOW = 'hideRegisterWindow',//скрыть окно регистрации
+    REGISTER_USER = 'registerUser',//пользователь регистрируется
+    REGISTER_USER_SUCCESS = 'registerUserSuccess',//пользователь успешно зарегистрировался
+    REGISTER_USER_FAIL = 'registerUserFail',//пользователь не зарегистрировался
 }
