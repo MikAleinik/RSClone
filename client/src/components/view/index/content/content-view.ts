d6ded697dab222afc45a285e83da30724d7322db
Mikhail Aleinik <6424156@tut.by>
2022-08-21 17:13:49 +0300

    feat: added localization on index page

--- client/src/components/view/index/content/content-view.ts
+++ client/src/components/view/index/content/content-view.ts
@@ -2,48 +2,66 @@ import './content.scss';
 import NewsView from "./news/news-view";
 import View from "../view";
 import Observer from "../../../controller/observer";
+import INotify from '../../../interfaces/i-notify';
+import ILocale from '../../../interfaces/i-locale';
+import { AppEvents } from '../../../controller/app-events';
+import LocaleModel from '../../../models/common/localization/locale-model';
+import { LocaleKeys } from '../../../models/common/localization/locale-keys';
 
-export default class ContentView extends View {
+export default class ContentView extends View implements INotify, ILocale {
     private readonly TAG_CONTAINER = 'main';
     private readonly TAG_PROMO = 'div';
+    private readonly TAG_HEADER = 'h3';
     private readonly TAG_LIST = 'ul';
     private readonly TAG_LIST_ITEM = 'li';
     private readonly CLASS_PROMO = 'promo';
 
-    private readonly TEXT_HEADER = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';//TODO (local) выносится в локализацию
-    private readonly TEXT_INFO_ONE = 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris';//TODO (local) выносится в локализацию
-    private readonly TEXT_INFO_TWO = 'Nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in';//TODO (local) выносится в локализацию
-    private readonly TEXT_INFO_THREE = 'Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla';//TODO (local) выносится в локализацию
-
     private _contentElement = document.createElement(this.TAG_CONTAINER);
+    private _headerListElement = document.createElement(this.TAG_HEADER);
+    private _listItemFirstElement = document.createElement(this.TAG_LIST_ITEM);
+    private _listItemSecondElement = document.createElement(this.TAG_LIST_ITEM);
+    private _listItemThirdElement = document.createElement(this.TAG_LIST_ITEM);
 
     constructor(observer: Observer) {
         super(observer);
         this.createContentElement();
+        this._observer.addSender(AppEvents.LOCALE_SET, this);
+        this._observer.notify(AppEvents.LOCALE_GET, this);
     }
     getCurrentElement(): HTMLElement {
         return this._contentElement;
     }
+    notify(nameEvent: AppEvents): AppEvents | void {
+        switch (nameEvent) {
+            case AppEvents.LOCALE_SET: {
+                this._observer.notify(AppEvents.LOCALE_GET, this);
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+    setLocale(locale: LocaleModel): void {
+        this._headerListElement.textContent = locale.getPhrase(LocaleKeys.PROMO_HEADER);
+        this._listItemFirstElement.textContent = locale.getPhrase(LocaleKeys.PROMO_FIRST);
+        this._listItemSecondElement.textContent = locale.getPhrase(LocaleKeys.PROMO_SECOND);
+        this._listItemThirdElement.textContent = locale.getPhrase(LocaleKeys.PROMO_THIRD);
+    }
     private createContentElement(): void {
-        let promoElement = this.createPromoElement();
+        let promoElement = document.createElement(this.TAG_PROMO);
+        promoElement.classList.add(this.CLASS_PROMO);
+
         let listElement = document.createElement(this.TAG_LIST);
-        listElement.insertAdjacentElement('beforeend', this.createListItemElement(this.TEXT_INFO_ONE));
-        listElement.insertAdjacentElement('beforeend', this.createListItemElement(this.TEXT_INFO_TWO));
-        listElement.insertAdjacentElement('beforeend', this.createListItemElement(this.TEXT_INFO_THREE));
+        listElement.insertAdjacentElement('beforeend', this._listItemFirstElement);
+        listElement.insertAdjacentElement('beforeend', this._listItemSecondElement);
+        listElement.insertAdjacentElement('beforeend', this._listItemThirdElement);
+
+        promoElement.insertAdjacentElement('beforeend', this._headerListElement);
         promoElement.insertAdjacentElement('beforeend', listElement);
+
         let newsElement = new NewsView(this._observer);
         this._contentElement.insertAdjacentElement('beforeend', promoElement);
         this._contentElement.insertAdjacentElement('beforeend', newsElement.getCurrentElement());
-    }
-    private createPromoElement(): HTMLElement {
-        let contentElement = document.createElement(this.TAG_PROMO);
-        contentElement.classList.add(this.CLASS_PROMO);
-        contentElement.textContent = this.TEXT_HEADER;//TODO не есть хорошо - сделать взамест заголовок страницы, а ля для сканеров поисковиков
-        return contentElement;
-    }
-    private createListItemElement(text: string): HTMLElement {
-        let listItemElement = document.createElement(this.TAG_LIST_ITEM);
-        listItemElement.textContent = text;
-        return listItemElement;
     }
 }
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,13 +1,15 @@
 import { AppEvents } from "./components/controller/app-events";
 import AuthController from "./components/controller/common/auth-controller";
 import NewsController from "./components/controller/index/news-controller";
-import RegisterController from "./components/controller/common/register-controller";
+import RegisterController from "./components/controller/index/register-controller";
 import Observer from "./components/controller/observer";
 import NewsModel from "./components/models/index/data-model/news-model";
 import UserModel from "./components/models/index/data-model/user-model";
 import AuthModel from "./components/models/index/state-model/auth-model";
 import RegisterModel from "./components/models/index/state-model/register-model";
 import IndexView from "./components/view/index/index-view";
+import LocaleModel from "./components/models/common/localization/locale-model";
+import LocaleController from "./components/controller/common/locale-controller";
 
 let observer = new Observer();
 
@@ -15,10 +17,12 @@ let authModel = new AuthModel();
 let registerModel = new RegisterModel();
 let userModel = new UserModel();
 let newsModel = new NewsModel();
+let localeModel = new LocaleModel();
 
 let authController = new AuthController(userModel, authModel);
 let registerController = new RegisterController(userModel, registerModel);
 let newsController = new NewsController(newsModel);
+let localeController = new LocaleController(localeModel);
 
 observer.addListener(AppEvents.AUTH_CLICK_BUTTON, authController)
     .addListener(AppEvents.AUTH_CLICK_LOGOUT_BUTTON, authController)
@@ -43,5 +47,8 @@ observer.addListener(AppEvents.AUTH_CLICK_BUTTON, authController)
     .addListener(AppEvents.REGISTER_USER, registerController)
     .addListener(AppEvents.REGISTER_USER_SUCCESS, registerController)
     .addListener(AppEvents.NEWS_GET_DATA, newsController)
+    .addListener(AppEvents.LOCALE_CHANGE, localeController)
+    .addListener(AppEvents.LOCALE_SET, localeController)
+    .addListener(AppEvents.LOCALE_GET, localeController)
 // .addListener(AppEvents.REGISTER_USER_FAIL, registerController);
 let app = new IndexView(observer);
