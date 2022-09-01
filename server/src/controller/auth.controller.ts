ed2618ed9f0fe8056d14093a612a0c370b9716ec
denis.borushko <denis.borushko@vizor-games.com>
2022-09-01 23:48:32 +0300

    fix: sequre off

--- server/src/controller/auth.controller.ts
+++ server/src/controller/auth.controller.ts
@@ -15,18 +15,18 @@ export class AuthController {
         algorithm: 'HS256',
         expiresIn: '1d',
     } as SignOptions;
-    // private static COOKIE_OPTIONS = {
-    //     path: '/',
-    //     httpOnly: true,
-    //     // secure: true,
-    //     sameSite: <boolean>(<unknown>'none'),
-    // };
     private static COOKIE_OPTIONS = {
         path: '/',
         httpOnly: true,
-        secure: true,
+        // secure: true,
         sameSite: <boolean>(<unknown>'none'),
     };
+    // private static COOKIE_OPTIONS = {
+    //     path: '/',
+    //     httpOnly: true,
+    //     secure: true,
+    //     sameSite: <boolean>(<unknown>'none'),
+    // };
     private static COOKIE_NAME = 'authCookie';
     private static COOKIE_UNAUTH = '-1';
     private static instance: AuthController;
